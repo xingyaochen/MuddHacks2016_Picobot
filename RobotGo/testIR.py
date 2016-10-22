b001ff5c04e949d32fb94f8c964b21119d4c4e35
16xchen <xingyao314@gmail.com>
2016-10-22 12:31:16 +0000

    added IR test

--- RobotGo/testIR.py
+++ RobotGo/testIR.py
@@ -24,13 +24,18 @@ io.setup(12, io.IN, pull_up_down=io.PUD_UP) #IR left
 io.setup(13, io.IN, pull_up_down=io.PUD_UP) #IR right
 front=io.input(7)
 back=io.input(11)
-right=io.input(12)
-left=io.input(13)
-if left==0:
-    print("left")
-if right==0:
-    print("left")
-if front==0:
-    print("left")
-if back==0:
-    print("left")
+left=io.input(12)
+right=io.input(13)
+print(front)
+print(back)
+print(right)
+print(left)
+while False:
+	if left==0:
+    		print("left")
+	if right==0:
+		print("right")
+	if front==0:
+		print("front")
+	if back==0:
+		print("back")
